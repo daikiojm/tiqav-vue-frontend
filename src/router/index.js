b96721948e433a89a1a5c7f691698b217f88a32e
daikiojm <daiking.ca2@gmail.com>
2017-10-15 16:05:24 +0900

    ルーティングと/→/newestへのリダイレクトを設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Newest from '@/components/container/Newest'
+import Random from '@/components/container/Random'
+import Search from '@/components/container/Search'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      redirect: '/newest'
+    },
+    {
+      path: '/newest',
+      name: 'Newest',
+      component: Newest
+    },
+    {
+      path: '/random',
+      name: 'Random',
+      component: Random
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
     }
   ]
 })
