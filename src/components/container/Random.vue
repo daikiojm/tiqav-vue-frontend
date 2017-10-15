65af90fd41d5453ffbe8f9de5c9184403bed9972
daikiojm <daiking.ca2@gmail.com>
2017-10-15 18:02:25 +0900

    axiosでjsonp取得を確認

--- src/components/container/Random.vue
+++ src/components/container/Random.vue
@@ -1,9 +1,31 @@
 <template>
+<div class="wapper">
   <p>Random</p>
+  <p>{{ results }}</p>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
+import jsonpAdapter from 'axios-jsonp'
+
 export default {
+  data () {
+    return {
+      results: ''
+    }
+  },
+  created: function () {
+    const resourceUrl = 'http://api.tiqav.com/search/random.json'
+    const config = { adapter: jsonpAdapter }
+    axios.get(resourceUrl, config)
+      .then((res) => {
+        this.results = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 }
 </script>
 
