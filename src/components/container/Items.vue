ef38f09c3b924570d68f28a00bcc26671ef8954c
daikiojm <daiking.ca2@gmail.com>
2017-10-17 08:46:19 +0900

    ファイル追加漏れ

---
+++ src/components/container/Items.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="_wapper">
+  <p>Items</p>
+  <el-card class="box-card">
+    <p>{{this.$route.params.id}}</p>
+  </el-card>
+
+</div>
+</template>
+
+<script>
+// import Vue from 'vue'
+// import axios from 'axios'
+// import jsonpAdapter from 'axios-jsonp'
+
+export default {
+  data () {
+    return {
+      results: ''
+    }
+  },
+  created: function () {
+    let id = this.$route.params.id
+    console.log(id)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+._wapper {
+  margin: 20px 10%;
+}
+</style>
